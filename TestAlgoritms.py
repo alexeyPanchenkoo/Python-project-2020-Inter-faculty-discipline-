5b8e96ccfcdf4597f2d5a969c67de201e5022f9d
Panchenko Aleksei <makovey090601@gmail.com>
2020-02-17 02:32:29 +0200

    trivial example to test the script

---
+++ TestAlgoritms.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from math import *
+def plus(x, y):
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
+    return print(x, " + ", y, " = ", x+y)
+plus(0,0) 
+
