dc608e112c986644227a727a31a9325e0cdfe31c
Panchenko Aleksei <makovey090601@gmail.com>
2020-02-17 03:32:49 +0200

    Add calculation of factorial and fibonachi number

--- TestAlgoritms.py
+++ TestAlgoritms.py
@@ -3,40 +3,65 @@ import math
 def plus(x, y):
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number: "))
-    return print(x, " + ", y, " = ", x+y)
+    return x+y #print(x, " + ", y, " = ", x+y)
 
 def minus(x, y):
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number: "))
-    return print(x, " - ", y, " = ", x-y)
+    return x-y #print(x, " - ", y, " = ", x-y)
 
 def multiply(x, y):
     x = int(input("Enter the first number: "))
     y = int(input("Enter the second number: "))
-    return print(x, " * ", y, " = ", x*y)
+    return x*y #print(x, " * ", y, " = ", x*y)
 
 def divide(x, y):
     x = int(input("Enter the dividend: "))
     y = int(input("Enter the divider: "))
-    return print(x, " / ", y, " = ", x/y)
+    return x/y #print(x, " / ", y, " = ", x/y)
 
 def power(x, y):
     x = int(input("Enter the number: "))
     y = int(input("Enter the power: "))
-    return print(x, " ^ ", y, " = ", x**y)
+    return x**y #print(x, " ^ ", y, " = ", x**y)
 
 def sqrt(x):
     x = int(input("Enter the number: "))
-    sqr = math.sqrt(x)
-    return print("Square root of", x, " = ", sqr)
+    return math.sqrt(x) #print("Square root of", x, " = ", sqr)
 
-plus(0,0)
-minus(0,0)
-multiply(0,0)
-divide(0,0)
-power(0,0)
-sqrt(0)
+def fact(x):
+    x = int(input("Enter the number: "))
+    s = 1     
+    while x > 0:
+        s = s * x
+        x = x - 1
+    return s   
+def fib(x):
+    x = int(input("Enter the fibonachi number: ")) 
+    fib1 = fib2 = 1
+    while x > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+        x = x - 1
+    return fib2    
 
+def out():
+    print("plus: ")
+    print( plus(0,0), "\n")
+    print("minus: ")
+    print(minus(0,0), "\n")
+    print("multiply: ")
+    print(multiply(0,0), "\n")
+    print("divide: ")
+    print(divide(0,0), "\n")
+    print("power: ")
+    print(power(0,0), "\n")
+    print("square root: ")
+    print(sqrt(0), "\n")
+    print("factorial:")
+    print(fact(0), "\n")
+    print("fibonachi: ")
+    print(fib(0), "\n")
+out()
 
 
 
