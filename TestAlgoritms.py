fd7d449f9853f6eab6d01435bb31b421d5f62ff5
Panchenko Aleksei <makovey090601@gmail.com>
2020-02-17 03:59:21 +0200

    Add all planed algoritms

--- TestAlgoritms.py
+++ TestAlgoritms.py
@@ -36,14 +36,36 @@ def fact(x):
         s = s * x
         x = x - 1
     return s   
+
 def fib(x):
-    x = int(input("Enter the fibonachi number: ")) 
+    x = int(input("Enter the fibonachi number: ")) - 1 
     fib1 = fib2 = 1
     while x > 0:
         fib1, fib2 = fib2, fib1 + fib2
         x = x - 1
     return fib2    
 
+def avOfList(x):
+    s = 0
+    x = list(map(int, input().split()))
+    for i in range(len(x)):
+         s = s + x[i]
+    return s/len(x)      
+
+def algProg(x, d, n):
+    x = int(input("Enter the first number: "))
+    d = int(input("Enter the algebraic progression step: "))
+    n = int(input("Enter the of desired members of the algebraic progression: "))
+    s = ((2 * x + (n - 1) * d) / 2) * n
+    return s
+
+def geoProg(x, q, n):
+    x = int(input("Enter the first number: "))
+    q = int(input("Enter the geometric progression step(not 1): "))
+    n = int(input("Enter the of desired members of the geometric progression: "))
+    s = (x * (1 - q**n))/(1-q)
+    return s
+    
 def out():
     print("plus: ")
     print( plus(0,0), "\n")
@@ -61,6 +83,15 @@ def out():
     print(fact(0), "\n")
     print("fibonachi: ")
     print(fib(0), "\n")
+    print("average of list: ")
+    print(avOfList(0), "\n")
+    print("sum of algebraic progression: ")
+    print(algProg(0, 0, 0), "\n")
+    print("sum of geometric progression: ")
+    print(geoProg(0, 0, 0), "\n")
+
+
+
 out()
 
 
