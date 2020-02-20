35767d0ea501688dbb92b46d5a3ffb98642d4782
Panchenko Aleksei <makovey090601@gmail.com>
2020-02-20 13:20:14 +0200

    care quit

--- Seting_window_Ikon_button/Window_Ikon.py
+++ Seting_window_Ikon_button/Window_Ikon.py
@@ -3,10 +3,9 @@
 
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication)
+    QPushButton, QApplication, QMessageBox)
 from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QIcon, QFont
 
 
 class Example(QWidget):
@@ -21,16 +20,25 @@ class Example(QWidget):
         QToolTip.setFont(QFont("SansSerif", 10))
         self.setToolTip('This is a <b>QWidget</b> widget')
 
-        btn = QPushButton('Quit', self)
+        btn = QPushButton('Fast quit', self)
         btn.clicked.connect(QCoreApplication.instance().quit)
         btn.resize(btn.sizeHint())
         btn.move(1200, 650)        
         self.setGeometry(300, 300, 1366, 768)
         self.setWindowTitle('Not dangerous math')
         self.setWindowIcon(QIcon('/home/alex/TestPySide/48.png'))
-
         self.show()
+	
+    def closeEvent(self, event):
 
+        reply = QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QMessageBox.Yes |
+            QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 if __name__ == '__main__':
 
